ecc98fb61b7ce5bff2c9f392644baa39aaf48d7f
Yeojin Park <godchn98@gmail.com>
2023-02-07 14:58:25 +0700

    #15-using querydsl, api search added
    using querydsl, api search added

--- simpleboard/src/main/java/com/simpleboard/simpleboard/repository/ArticleCommentRepository.java
+++ simpleboard/src/main/java/com/simpleboard/simpleboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,28 @@
 package com.simpleboard.simpleboard.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.simpleboard.simpleboard.domain.Article;
 import com.simpleboard.simpleboard.domain.ArticleComment;
+import com.simpleboard.simpleboard.domain.QArticle;
+import com.simpleboard.simpleboard.domain.QArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment> {
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content,  root.createdAt, root.createdBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
