91cf874ff076eb59b8eb4b34f22a76fb258b9993
Yeojin Park <godchn98@gmail.com>
2023-02-07 12:33:54 +0700

    #14 - data rest setup

--- simpleboard/src/main/java/com/simpleboard/simpleboard/repository/ArticleRepository.java
+++ simpleboard/src/main/java/com/simpleboard/simpleboard/repository/ArticleRepository.java
@@ -2,6 +2,9 @@ package com.simpleboard.simpleboard.repository;
 
 import com.simpleboard.simpleboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
